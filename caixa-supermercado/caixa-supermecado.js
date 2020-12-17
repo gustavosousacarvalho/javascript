a3a537090eef1cb227614a5b41d60b50943b8603
gustavosousacarvalho <gc49862@gmail.com>
2020-12-17 20:41:02 -0200

    Inicio javascript

---
+++ caixa-supermercado/caixa-supermecado.js
@@ -0,0 +1,20 @@
+var produtos = [
+    {
+        id: 1,
+        nome: "coca cola lata 350ml",
+        valor:7.00,
+        quantidaEstoque:60,
+        img:"imgs/coca-cola.jpg"
+    },
+    {
+        id: 2,
+        nome: "feijão camil 1kg",
+        valor:8.00,
+        quantidaEstoque:25,
+        img:"feijao-camil-1kg.jpg"
+    }
+]
+
+window.onload=function(){
+   var select= document.getElementsByTagName("select");
+}
