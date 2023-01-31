7f02b4be4443f8ffef05d8cd7902422b3c7e84d3
gustavosousacarvalho <gc49862@gmail.com>
2023-01-31 17:06:35 -0300

    inicio

---
+++ loga-camisas/j.js
@@ -0,0 +1,20 @@
+const itens = [
+    {
+        id : 0,
+        nome : 'camisa de time',
+        img : 'https://imgcentauro-a.akamaihd.net/900x900/M0EYOY60/camisa-do-flamengo-i-21-22-torcedor-adidas-masculina-img.jpg',
+        quantidade: 0,
+    },
+    {
+        id : 1,
+        nome : 'short',
+        img : 'https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/98f0dbebd4014f618032ae3c01737fe8_9366/shorts-1-cr-flamengo-22-23.jpg',
+        quantidade: 0,
+    },
+    {
+        id : 2,
+        nome : 'chuteira',
+        img : 'https://imgcentauro-a.akamaihd.net/600x600/96051819/chuteira-society-adidas-deportivo-tf-adulto-img.jpg',
+        quantidade : 0,
+    }
+]
